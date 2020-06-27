ae6a94ec3fdcbcfd45b12bd6e47196e571dd04ec
codewithgsp <codewithgsp@gmail.com>
2020-06-27 18:03:56 +0200

    simple banking system - stage -2

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -6,10 +6,11 @@ class SimpleBankingSystem:
 
     def __init__(self):
         self.login_menu_options = {1: 'Create an account', 2: 'Log into account', 0: 'Exit'}
-        self.iin = 400000
+        self.iin = '400000'
         self.logged_in_menu_option = {1: 'Balance', 2: 'Logout', 0: 'Exit'}
         self.balance = 0
         self.card_pool = {}
+        self.acc_pool = set()
 
     def display_option(self, options):
         for option in options:
@@ -18,14 +19,27 @@ class SimpleBankingSystem:
     def fetch_user_option(self):
         return input()
 
-    def create_new_card_number(self):
+    def calculate_checksum(self, acc, iin):
+        unique_15_digit = list(map(int, iin + acc))
+        for i, digit in enumerate(unique_15_digit):
+            if (i + 1) % 2 == 1:
+                unique_15_digit[i] = (digit * 2) - 9 if digit * 2 > 9 else digit * 2
+        return str(10 - (sum(unique_15_digit) % 10)) if sum(unique_15_digit) % 10 != 0 else '0'
+
+    def create_unique_acc_num(self):
         while True:
             acc = str(random.randint(0, 999999999)).zfill(9)
-            checksum = random.randint(0, 9)
-            card_number = str(self.iin) + acc + str(checksum)
-            if card_number not in self.card_pool:
-                self.card_pool[card_number] = str(random.randint(0, 9999)).zfill(4)
-                return card_number
+            if acc not in self.acc_pool:
+                self.acc_pool.add(acc)
+                return acc
+
+    def create_new_card_number(self):
+        unique_acc = self.create_unique_acc_num()
+        checksum = self.calculate_checksum(unique_acc, self.iin)
+        card_number = self.iin + unique_acc + checksum
+        if card_number not in self.card_pool:
+            self.card_pool[card_number] = str(random.randint(0, 9999)).zfill(4)
+            return card_number
 
     def create_an_account(self):
         print('\nYour card has been created')
