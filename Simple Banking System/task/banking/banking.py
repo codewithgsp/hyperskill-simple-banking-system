869a6ec33bd6abf22ab1f234ea798f390e23f232
codewithgsp <codewithgsp@gmail.com>
2020-06-29 00:52:15 +0200

    simple banking system - stage -4 REVISED CODE

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -17,11 +17,7 @@ class SimpleBankingSystem:
                                       4: 'Close account',
                                       5: 'Log out',
                                       0: 'Exit'}
-        # self.balance = 0
-        # self.card_pool = {}
-        # self.acc_pool = set()
         self.conn = None
-        # self.c = None
         self.user_input_acc = None
         self.user_input_pin = None
 
@@ -55,18 +51,19 @@ class SimpleBankingSystem:
     def insert_new_card_details(self, conn, number, pin):
         c = conn.cursor()
         c.execute(''' INSERT INTO card(number, pin)
-                           VALUES({number}, {pin}) '''.format(number=number, pin=pin))
-        # self.commit_records()
+                      VALUES({number}, {pin}) '''.format(number=number, pin=pin))
+        self.commit_records()
 
     def commit_records(self):
         self.conn.commit()
 
     def get_card_details(self, conn, acc):
         c = conn.cursor()
-        c.execute(''' SELECT substr(number, 7, 9)
-                           FROM card
-                           WHERE substr(number, 7, 9) = '{acc}' '''.format(acc=acc))
+        c.execute(''' SELECT number
+                      FROM card
+                      WHERE number = '{acc}' '''.format(acc=acc))
         card_detail = c.fetchone()
+        print(card_detail[0])
         if not card_detail:
             return None
         return card_detail[0]
@@ -74,9 +71,10 @@ class SimpleBankingSystem:
     def get_pin_details(self, conn, number):
         c = conn.cursor()
         c.execute(''' SELECT pin
-                           FROM card
-                           WHERE substr(number, 7, 9) = '{number}' '''.format(number=number))
+                      FROM card
+                      WHERE number = '{number}' '''.format(number=number))
         pin_detail = c.fetchone()
+        print(pin_detail[0])
         if not pin_detail:
             return None
         return pin_detail[0]
@@ -84,33 +82,48 @@ class SimpleBankingSystem:
     def show_balance(self, conn, number):
         c = conn.cursor()
         c.execute(''' SELECT balance
-                           FROM card
-                           WHERE number = '{number}' '''.format(number=number))
+                      FROM card
+                      WHERE number = '{number}' '''.format(number=number))
         balance_detail = c.fetchone()
         return balance_detail[0]
 
-    def update_balance(self, conn, number, amount):
+    def credit_balance(self, conn, number, amount):
         c = conn.cursor()
         c.execute(''' UPDATE card
-                           SET balance = {amount} 
-                           WHERE number = '{number}' '''.format(number=number, amount=amount))
+                      SET balance = balance + {amount} 
+                      WHERE number = '{number}' '''.format(number=number, amount=amount))
+        self.commit_records()
+
+    def debit_balance(self, conn, number, amount):
+        c = conn.cursor()
+        c.execute(''' UPDATE card
+                      SET balance = balance - {amount} 
+                      WHERE number = '{number}' '''.format(number=number, amount=amount))
         self.commit_records()
 
     def close_account(self, conn, number):
         c = conn.cursor()
-        c.execute(''' DELETE FROM card
-                           WHERE number = {number} '''.format(number=number))
+        c.execute(''' DELETE 
+                      FROM card
+                      WHERE number = '{number}' '''.format(number=number))
         self.conn.commit()
         print('The account has been closed!')
 
     def verify_transfer_acc_no(self, transfer_acc_no):
-        card_number, checksum = self.verify_login_details_from_db(transfer_acc_no[6:15], 'transfer')
+
+        if transfer_acc_no[-1] != self.calculate_checksum(transfer_acc_no[6:15], transfer_acc_no[:6]):
+            print('Probably you made mistake in the card number. Please try again!')
+            return False
+
+        card_number = self.verify_login_details_from_db(transfer_acc_no[6:15], transfer_acc_no[:6], 'transfer')
         if card_number is None:
             print('Such a card does not exist.')
             return False
-        elif self.iin + card_number == transfer_acc_no[:15] and checksum != transfer_acc_no[-1]:
-            print('Probably you made mistake in the card number. Please try again!')
+
+        if card_number == transfer_acc_no:
+            print("You can't transfer money to the same account!")
             return False
+
         return True
 
     def perform_transfer(self):
@@ -121,9 +134,8 @@ class SimpleBankingSystem:
             if int(transfer_amount) > current_balance:
                 print('Not enough money!')
             else:
-                self.update_balance(self.conn, self.user_input_acc, current_balance - int(transfer_amount))
-                self.update_balance(self.conn, transfer_acc_no, self.show_balance(self.conn, transfer_acc_no)
-                                    + int(transfer_amount))
+                self.debit_balance(self.conn, self.user_input_acc, int(transfer_amount))
+                self.credit_balance(self.conn, transfer_acc_no, int(transfer_amount))
                 print('Success!')
 
     def display_option(self, options):
@@ -140,18 +152,21 @@ class SimpleBankingSystem:
                 unique_15_digit[i] = (digit * 2) - 9 if digit * 2 > 9 else digit * 2
         return str(10 - (sum(unique_15_digit) % 10)) if sum(unique_15_digit) % 10 != 0 else '0'
 
+    def create_unique_pin_num(self):
+        return str(random.randint(0, 9999)).zfill(4)
+
     def create_unique_acc_num(self):
-        while True:
-            acc = str(random.randint(0, 999999999)).zfill(9)
-            acc_check_db = self.get_card_details(self.conn, acc)
-            if acc_check_db is None:
-                return acc
+        return str(random.randint(0, 999999999)).zfill(9)
 
     def create_new_card_number(self):
-        unique_acc = self.create_unique_acc_num()
-        checksum = self.calculate_checksum(unique_acc, self.iin)
-        card_number = self.iin + unique_acc + checksum
-        return card_number, str(random.randint(0, 9999)).zfill(4)
+        while True:
+            unique_acc = self.create_unique_acc_num()
+            checksum = self.calculate_checksum(unique_acc, self.iin)
+            card_number = self.iin + unique_acc + checksum
+            # check in db
+            check_number_db = self.get_card_details(self.conn, card_number)
+            if check_number_db is None:
+                return card_number, self.create_unique_pin_num()
 
     def create_an_account(self):
         print('\nYour card has been created')
@@ -164,18 +179,23 @@ class SimpleBankingSystem:
         print('Your card number:\n{}'.format(new_card_number))
         print('Your card PIN:\n{}'.format(new_card_pin))
 
-    def verify_login_details_from_db(self, acc_number, transaction_type):
-        card_number = self.get_card_details(self.conn, acc_number)
-        card_pin = self.get_pin_details(self.conn, acc_number)
-        checksum = self.calculate_checksum(acc_number, self.iin)
+    def verify_login_details_from_db(self, acc_number, iin, transaction_type):
+
+        checksum = self.calculate_checksum(acc_number, iin)
+        number = iin + acc_number + checksum
+        # from db
+        card_number = self.get_card_details(self.conn, number)
+        card_pin = self.get_pin_details(self.conn, number)
+
         if transaction_type == 'login':
-            return card_number, card_pin, checksum
-        return card_number, checksum
+            return card_number, card_pin
+        return card_number
 
     def verify_login_details(self, number, pin):
         acc_number = number[6:15]
-        card_number, card_pin, checksum = self.verify_login_details_from_db(acc_number, 'login')
-        if acc_number == card_number and pin == card_pin and checksum == number[-1]:
+        iin = number[:6]
+        card_number, card_pin = self.verify_login_details_from_db(acc_number, iin, 'login')
+        if number == card_number and pin == card_pin:
             print('\nYou have successfully logged in!')
             return True
         print('\nWrong card number or PIN!')
@@ -190,9 +210,8 @@ class SimpleBankingSystem:
             if choice == '1':
                 print('\nBalance: {}'.format(self.show_balance(self.conn, self.user_input_acc)))
             elif choice == '2':
-                current_balance = self.show_balance(self.conn, self.user_input_acc)
                 amount = self.fetch_user_option('\nEnter income:')
-                self.update_balance(self.conn, self.user_input_acc, current_balance + int(amount))
+                self.credit_balance(self.conn, self.user_input_acc, int(amount))
                 print('Income was added!')
             elif choice == '3':
                 self.perform_transfer()
